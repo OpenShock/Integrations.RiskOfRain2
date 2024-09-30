567a476ab8e2d00d3965661598d31e7ce6740d85
Luca <luca@perugia.ovh>
2024-09-30 15:04:30 +0200

    Initial commit

---
+++ Integrations.RiskOfRain2/RiskOfPain.cs
@@ -0,0 +1,24 @@
+﻿using BepInEx;
+using Newtonsoft.Json;
+using RoR2;
+
+namespace Integrations.RiskOfRain2;
+
+[BepInPlugin("OpenShock.Integrations.RiskOfRain2", "RiskOfPain", "1.0.0")]
+public class RiskOfPain : BaseUnityPlugin
+{
+    private void OnEnable()
+    {
+        GlobalEventManager.onClientDamageNotified += ClientOnDamage;
+    }
+
+    private void OnDisable()
+    {
+        GlobalEventManager.onClientDamageNotified -= ClientOnDamage;
+    }
+
+    private void ClientOnDamage(DamageDealtMessage damageMessage)
+    {
+        
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# RiskOfPain (Integrations.RiskOfRain2)
