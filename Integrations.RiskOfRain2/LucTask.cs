77b4f232e6d3936649a49c8d8cea38e7c1fe2d1c
Luca <luca@perugia.ovh>
2024-10-01 17:40:09 +0200

    Its alive

---
+++ Integrations.RiskOfRain2/LucTask.cs
@@ -0,0 +1,18 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace OpenShock.Integrations.RiskOfRain2;
+
+public static class LucTask
+{
+    public static Task Run(Func<Task?> function, CancellationToken token = default, [CallerFilePath] string file = "",
+        [CallerMemberName] string member = "", [CallerLineNumber] int line = -1) => Task.Run(function, token)
+        .ContinueWith(
+            t =>
+            {
+                if (!t.IsFaulted) return;
+                var index = file.LastIndexOf('\\');
+                if (index == -1) index = file.LastIndexOf('/');
+                RiskOfPain.ModLogger.LogError(
+                    $"Error during task execution. {file.Substring(index + 1, file.Length - index - 1)}::{member}:{line} {t.Exception}");
+            }, TaskContinuationOptions.OnlyOnFaulted);
+}
--- Integrations.RiskOfRain2/RiskOfPain.cs
+++ Integrations.RiskOfRain2/RiskOfPain.cs
@@ -1,20 +1,157 @@
 ﻿using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
 using OpenShock.Integrations.LethalCompany.OpenShockApi.Models;
 using OpenShock.Integrations.RiskOfRain2.OpenShockApi.Models;
+using R2API.Utils;
+using RiskOfOptions;
+using RiskOfOptions.OptionConfigs;
+using RiskOfOptions.Options;
 using RoR2;
 
 namespace OpenShock.Integrations.RiskOfRain2;
 
 [BepInPlugin(MyPluginInfo.PLUGIN_GUID, "RiskOfPain", MyPluginInfo.PLUGIN_VERSION)]
+[NetworkCompatibility(CompatibilityLevel.NoNeedForSync)]
+[BepInDependency("com.rune580.riskofoptions")]
 public class RiskOfPain : BaseUnityPlugin
 {
-    private const string ActualVersion = "1.0.0";
-    private readonly Version _version = new Version(ActualVersion);
+    public static RiskOfPain Instance { get; private set; } = null!;
+    public static ManualLogSource ModLogger { get; private set; } = null!;
+
+    private OpenShockApi.OpenShockApi? _openShockApiClient = null;
+    private IList<Guid> _shockersList = null!;
+
+    // OpenShock Server
+    private ConfigEntry<string> _openShockServer = null!;
+    private ConfigEntry<string> _openShockApiToken = null!;
+    private ConfigEntry<string> _shockers = null!;
+
+    // Settings
+
+    // OnDeath
+    private ConfigEntry<byte> _settingOnDeathIntensity = null!;
+    private ConfigEntry<ushort> _settingOnDeathDuration = null!;
+
+    // OnDamage
+    private ConfigEntry<bool> _settingOnDamageEnabled = null!;
+    private ConfigEntry<ControlType> _settingOnDamageMode = null!;
+
+    private ConfigEntry<int> _settingOnDamageDuration = null!;
+    private ConfigEntry<int> _settingOnDamageIntensityLimit = null!;
 
     private void Awake()
     {
-        Logger.LogDebug("RiskOfPain loaded...");
-        
+        Logger.LogDebug("RiskOfPain loading...");
+
+        Instance = this;
+        ModLogger = Logger;
+
+        // Config
+
+        // OpenShock Server
+        _openShockServer = Config.Bind<string>("OpenShock", "Server", "https://api.openshock.app",
+            "The URL of the OpenShock backend");
+        _openShockApiToken = Config.Bind<string>("OpenShock", "ApiToken", "",
+            "API token for authentication, can be found under API Tokens on the OpenShock dashboard (https://shocklink.net/#/dashboard/tokens)");
+        _shockers = Config.Bind<string>("Shockers", "Shockers", "comma,seperated,list,of,shocker,ids",
+            "A list of shocker IDs to use within the mod. Comma seperated.");
+
+
+        // Settings
+
+        // OnDeath
+        _settingOnDeathIntensity = Config.Bind<byte>("OnDeath", "Intensity", 25,
+            "The intensity of the shocker when the player dies");
+        _settingOnDeathDuration = Config.Bind<ushort>("OnDeath", "Duration", 1000,
+            "The duration of the shocker when the player dies");
+
+        // OnDamage
+
+        _settingOnDamageEnabled = Config.Bind<bool>("OnDamage", "Enabled", true, "Enables on damage");
+        _settingOnDamageMode = Config.Bind<ControlType>("OnDamage", "Mode", ControlType.Shock,
+            "The action that happens when you take damage");
+
+        _settingOnDamageDuration = Config.Bind<int>("OnDamage", "Duration", 100,
+            "The duration of the shocker when the player takes damage");
+        _settingOnDamageIntensityLimit = Config.Bind<int>("OnDamage", "IntensityLimit", 25,
+            "Intensity limit for the shocker when the player takes damage");
+
+        _openShockServer.SettingChanged += UpdateApiClient;
+        _openShockApiToken.SettingChanged += UpdateApiClient;
+        _shockers.SettingChanged += ShockersOnSettingChanged;
+
+        SetupApiClient();
+        SetupShockers();
+
+        ModSettingsManager.AddOption(new StringInputFieldOption(_openShockServer));
+        ModSettingsManager.AddOption(new StringInputFieldOption(_openShockApiToken, new InputFieldConfig()
+        {
+            submitOn = InputFieldConfig.SubmitEnum.OnExitOrSubmit
+        }));
+        ModSettingsManager.AddOption(new StringInputFieldOption(_shockers, new InputFieldConfig()
+        {
+            submitOn = InputFieldConfig.SubmitEnum.OnExitOrSubmit
+        }));
+
+        ModSettingsManager.AddOption(new CheckBoxOption(_settingOnDamageEnabled));
+        ModSettingsManager.AddOption(new ChoiceOption(_settingOnDamageMode));
+        ModSettingsManager.AddOption(new IntSliderOption(_settingOnDamageDuration, new IntSliderConfig
+        {
+            min = 300,
+            max = 30_000
+        }));
+        ModSettingsManager.AddOption(new IntSliderOption(_settingOnDamageIntensityLimit, new IntSliderConfig
+        {
+            min = 1,
+            max = 100
+        }));
+
+
+        Logger.LogDebug("RiskOfPain loaded");
+    }
+
+    private void ShockersOnSettingChanged(object sender, EventArgs e)
+    {
+        SetupShockers();
+    }
+
+    private void SetupShockers()
+    {
+        var newList = new List<Guid>();
+        foreach (var shocker in _shockers.Value.Split(','))
+        {
+            if (Guid.TryParse(shocker, out var shockerGuid))
+            {
+                Logger.LogInfo("Found shocker ID " + shockerGuid);
+                newList.Add(shockerGuid);
+            }
+            else Logger.LogError($"Failed to parse shocker ID {shocker}");
+        }
+
+        _shockersList = newList;
+    }
+
+    private void UpdateApiClient(object sender, EventArgs e)
+    {
+        SetupApiClient();
+    }
+
+    private void SetupApiClient()
+    {
+        if (!Uri.TryCreate(_openShockServer.Value, UriKind.Absolute, out var serverUri))
+        {
+            Logger.LogWarning("Unable to parse OpenShock server URL. E.g. https://api.openshock.app");
+            return;
+        }
+
+        if (string.IsNullOrWhiteSpace(_openShockApiToken.Value))
+        {
+            Logger.LogWarning("API Token is not configured");
+            return;
+        }
+
+        _openShockApiClient = new OpenShockApi.OpenShockApi(serverUri, _openShockApiToken.Value);
     }
 
     private void OnEnable()
@@ -23,34 +160,6 @@ public class RiskOfPain : BaseUnityPlugin
         CharacterBody.onBodyDestroyGlobal += OnCharacterBodyDestroy;
         GlobalEventManager.onClientDamageNotified += ClientOnDamage;
         Logger.LogDebug("Events registered");
-
-        var openShockApiClient = new OpenShockApi.OpenShockApi(new Uri("https://api.openshock.app/"), ""); 
-        
-        
-        Task.Run(async () =>
-        {
-            try
-            {
-                Logger.LogDebug("Sending control request");
-                var shocks = new List<Control>();
-                shocks.Add(new Control()
-                {
-                    Id = Guid.Parse("d9267ca6-d69b-4b7a-b482-c455f75a4408"),
-                    Duration = 1000,
-                    Intensity = (byte)Math.Clamp(50, 0, 100),
-                    Type = ControlType.Vibrate
-                });
-                await openShockApiClient.Control(shocks);
-
-                Logger.LogDebug("control response");
-            }
-            catch (Exception e)
-            {
-                Logger.LogError(e.ToString());
-            }
-        });
-        
-        Logger.LogDebug("OpenShockApiClient registered");
     }
 
     private void OnDisable()
@@ -85,18 +194,34 @@ public class RiskOfPain : BaseUnityPlugin
 
     private void ClientOnDamage(DamageDealtMessage damageMessage)
     {
-        
-        try
+        if (_localUserCharacterBody == null) return;
+        if (damageMessage.victim != _localUserCharacterBody.gameObject) return;
+
+        Logger.LogDebug($"Player received damage: {damageMessage.damage}");
+
+
+        LucTask.Run(async () =>
         {
-            if (_localUserCharacterBody == null) return;
-            if (damageMessage.victim != _localUserCharacterBody.gameObject) return;
-            
-            Logger.LogError("YOOO WE RECEIVED DAMAGE TO THE RISK OF RAIN! - " + damageMessage.victim.name + " - " +
-                            damageMessage.damage);
-        }
-        catch (Exception e)
-        {
-            Logger.LogError(e.ToString());
-        }
+            if (_openShockApiClient == null)
+            {
+                Logger.LogWarning("OpenShock server or token not configured");
+                return;
+            }
+
+            Logger.LogDebug("Sending control request");
+
+            var controls = _shockersList.Select(shocker =>
+                new Control
+                {
+                    Id = shocker,
+                    Duration = (ushort)_settingOnDamageDuration.Value,
+                    Intensity = (byte)_settingOnDamageIntensityLimit.Value,
+                    Type = _settingOnDamageMode.Value
+                });
+
+            await _openShockApiClient.Control(controls);
+
+            Logger.LogDebug("");
+        });
     }
 }
